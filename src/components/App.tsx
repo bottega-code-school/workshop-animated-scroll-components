b385bf7eb6e8639962c1cf512958c3a396194df7
Jordan Hudgens <jordan@devcamp.com>
2022-04-06 15:48:37 -0700

    Updated final version

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,10 @@ import Layout from "./Layout";
 import "animate.css/animate.min.css";
 import { AnimationOnScroll } from "react-animation-on-scroll";
 import { Element, Link } from "react-scroll";
+import {
+  BsFillArrowDownSquareFill,
+  BsFillArrowUpSquareFill,
+} from "react-icons/bs";
 
 const SCROLL_OFFSET = 0;
 const SCROLL_SPEED = 1000;
@@ -13,26 +17,66 @@ const App = () => {
     <Layout>
       <div className="content__blocks">
         {nums.map((num) => (
-          <div key={num} className="content__block">
-            <div>
-              <h1>Block {num}</h1>
-              <p>
-                Nullam quis risus eget urna mollis ornare vel eu leo. Cras justo
-                odio, dapibus ac facilisis in, egestas eget quam. Donec id elit
-                non mi porta gravida at eget metus. Cras justo odio, dapibus ac
-                facilisis in, egestas eget quam. Maecenas sed diam eget risus
-                varius blandit sit amet non magna. Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit. Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit. Integer posuere erat a ante
-                venenatis dapibus posuere velit aliquet. Etiam porta sem
-                malesuada magna mollis euismod. Donec ullamcorper nulla non
-                metus auctor fringilla. Fusce dapibus, tellus ac cursus commodo,
-                tortor mauris condimentum nibh, ut fermentum massa justo sit
-                amet risus. Etiam porta sem malesuada magna mollis euismod.
-                Maecenas sed diam eget risus varius blandit sit amet non magna.
-              </p>
-            </div>
-          </div>
+          <Element
+            key={num}
+            name={num.toString()}
+            style={{ height: "calc(100vh - 150px)", paddingTop: "150px" }}
+          >
+            <AnimationOnScroll
+              animateIn="animate__backInRight"
+              animateOut="animate__backOutRight"
+              animateOnce={false}
+            >
+              <div key={num} className="content__block">
+                <div className="content__block__actions">
+                  {num !== 0 ? (
+                    <Link
+                      style={{ cursor: "pointer" }}
+                      className="content__block__actions--item"
+                      to={(num - 1).toString()}
+                      offset={SCROLL_OFFSET}
+                      duration={SCROLL_SPEED}
+                      spy
+                      smooth
+                    >
+                      <BsFillArrowUpSquareFill />
+                    </Link>
+                  ) : (
+                    <div className="content__block__actions--item" />
+                  )}
+
+                  {num !== nums.length - 1 ? (
+                    <Link
+                      style={{ cursor: "pointer" }}
+                      className="content__block__actions--item"
+                      to={(num + 1).toString()}
+                      offset={SCROLL_OFFSET}
+                      duration={SCROLL_SPEED}
+                      spy
+                      smooth
+                    >
+                      <BsFillArrowDownSquareFill />
+                    </Link>
+                  ) : (
+                    <div className="content__block__actions--item" />
+                  )}
+                </div>
+                <div>
+                  <h1>Block {num}</h1>
+                  <p>
+                    Nullam quis risus eget urna mollis ornare vel eu leo. Cras
+                    justo odio, dapibus ac facilisis in, egestas eget quam.
+                    Donec id elit non mi porta gravida at eget metus. Cras justo
+                    odio, dapibus ac facilisis in, egestas eget quam. Maecenas
+                    sed diam eget risus varius blandit sit amet non magna. Lorem
+                    ipsum dolor sit amet, consectetur adipiscing elit. Lorem
+                    ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                    posuere erat a ante venenatis dapibus posuere velit aliquet.
+                  </p>
+                </div>
+              </div>
+            </AnimationOnScroll>
+          </Element>
         ))}
       </div>
     </Layout>
